958fd9d99fd686e7e9808c9fad384a5c36b420b2
João Vitor <joaovitorleffa02@gmail.com>
2021-09-18 19:36:49 -0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.SERVER_PORT || 3000);
 }
